679ba56445ac051d0b3fef89c89db26fa37b1972
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-05-19 19:31:47 -0500

    [+] index.js: Definir 3 rutas para desplegar 3 URLs en el navegador

--- index.js
+++ index.js
@@ -2,6 +2,18 @@
 import express from 'express';              // Importacion de dependencia usando ESModule
 const app = express();                      // Invocando la ejecucion de Express
 
+// Endpoint: http://localhost:3000/
+app.get( '/', ( req, res ) => {
+    res.send( '<h1>Home</h1>' );
+} );
+app.get( '/about-us', ( req, res ) => {
+    res.send( '<h1>About us</h1>' );
+} );
+app.get( '/contact', ( req, res ) => {
+    res.send( '<h1>Contact</h1>' );
+} );
+
+// listen: Lanzar el servidor en http://localhost:3000
 app.listen( 3000, () => {
     console.log( 'Servidor corriendo en http://localhost:3000' );
 } );
