7787f8f6920844752d6df0149fde39c0f64c5efe
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-05-19 19:06:32 -0500

    [+] package.json: Configurar ESModule [+] index.js: Cambia importacion a ESModule

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require( 'express' );       // Importacion de dependencia usando CommonJS
+// const express = require( 'express' );    // Importacion de dependencia usando CommonJS
+import express from 'express';              // Importacion de dependencia usando ESModule
 const app = express();                      // Invocando la ejecucion de Express
 
 app.listen( 3000, () => {
