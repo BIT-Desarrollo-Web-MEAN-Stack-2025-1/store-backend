75e151a544d91773bf110477b1434257639068b5
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-05-29 12:15:41 -0500

    [+] user.router: GET, PATCH, DELETE /api/users/:id [+] user.controller: Define getUserById(), updateUserById(), deleteUserById()

--- src/controllers/user.controller.mjs
+++ src/controllers/user.controller.mjs
@@ -27,8 +27,98 @@ const createUser = async ( req, res ) => {
 
 }
 
+const getAllUsers = async ( req, res ) => {
+
+    try {
+        const data = await userModel.find({});
+        res.status( 200 ).json( data );
+    } 
+    catch ( error ) {
+        console.error( error );
+        res.status( 500 ).json({ msg: 'Error: No se pudo obtener el listado de usuarios' });
+    }
+
+}
+
+const getUserById = async ( req, res ) => {
+    const userId = req.params.id;
+
+    try {
+        const data = await userModel.findById( userId );
+
+        // data == null --> equivale a --> ! data
+        if( ! data ) {
+            return res.json({ msg: 'Usuario no registrado' });
+        }
+
+        res.json( data );
+    } 
+    catch ( error ) {
+        console.error( error );
+        res.json({ msg: 'Error: No pudo obtener el usuario por ID' });
+    }
+
+}
+
+const updateUserById = async ( req, res ) => {
+    const userId = req.params.id;
+    const inputData = req.body;
+
+    try {
+        // Paso 1: Verificar si el usuario existe
+        const userFound = await userModel.findOne({ 
+            username: inputData.username,
+            email: inputData.email
+        });
+
+        // userFound == null --> equivale a --> ! userFound
+        if( ! userFound ) {
+            return res.status( 404 ).json({ msg: 'El usuario que deseas actualizar no existe' });
+        }
+
+        // Paso 2: Actualizar el usuario si existe
+        const data = await userModel.findByIdAndUpdate( userId, inputData, { new: true } );
+        res.status( 200 ).json( data );
+    } 
+    catch ( error ) {
+        console.error( error );
+        res.status( 500 ).json({ msg: 'Error: No pudo actualizar el usuario' });
+    }
+
+}
+
+const deleteUserById = async ( req, res ) => {
+    const userId = req.params.id;
+
+    try {
+        // Paso 1: Verificar si el usuario existe por ID
+        const userFound = await userModel.findOne({
+            _id: userId
+        });
+
+        // userFound == null --> equivale a --> ! userFound
+        if( ! userFound ) {
+            return res.status( 404 ).json({ msg: 'El usuario que deseas eliminar no existe' });
+        }
+
+        // Paso 2: Eliminar el usuario por ID
+        const data = await userModel.findOneAndDelete({ _id: userId });
+
+        res.status( 200 ).json( data );
+    } 
+    catch ( error ) {
+        console.error( error );
+        res.status( 500 ).json({ msg: 'Error: No puede eliminar el usuario por ID' });
+    }
+
+}
+
 
 // Exponiendo las funcionalidades de este archivo
 export {
-    createUser
+    createUser,
+    getAllUsers,
+    getUserById,
+    updateUserById,
+    deleteUserById
 }
--- src/routes/user.route.mjs
+++ src/routes/user.route.mjs
@@ -1,10 +1,16 @@
 import express from 'express';
-import { createUser } from '../controllers/user.controller.mjs';
+import { createUser, deleteUserById, getAllUsers, getUserById, updateUserById } from '../controllers/user.controller.mjs';
 
 const router = express.Router();
 
 // Define las rutas para la entidad user
 router.post( '/api/users', createUser );
+router.get( '/api/users', getAllUsers );
+
+// Parametrizar las rutas
+router.get( '/api/users/:id', getUserById );
+router.patch( '/api/users/:id', updateUserById );
+router.delete( '/api/users/:id', deleteUserById );
 
 
 export default router;
