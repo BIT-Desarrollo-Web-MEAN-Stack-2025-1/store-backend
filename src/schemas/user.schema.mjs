c392b23c01cfc10e5642d72d083e1ea57a368078
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-06-04 19:28:52 -0500

    [+] product.schema: Define estructura [+] user.schema: Soluciona conflictos

--- src/schemas/user.schema.mjs
+++ src/schemas/user.schema.mjs
@@ -1,21 +1,6 @@
 import mongoose from 'mongoose';
 
-<<<<<<< HEAD
 // Define la estructura del documento que se va a registrar
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: [ true, 'El nombre del usuario es obligatorio'  ]      // Obligatorio
-    },
-    email: {
-        type: String,
-        trim: true,
-        lowercase: true,
-        unique: true,
-        required: [ true, 'El correo del usuario es obligatorio' ],
-=======
-
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -35,39 +20,14 @@ const userSchema = new mongoose.Schema({
         unique: true,
         trim: true,
         lowercase: true,
->>>>>>> feature/authentication
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Por favor, introduce un correo electrónico válido.']
     },
     password: {
         type: String,
         trim: true,
-<<<<<<< HEAD
-        min: [ 6, 'La contrasena debe tener al menos 6 caracteres' ],
-        max: [ 12, 'La contrasena debe tener maxico 12 caracteres' ],
+        // min: [ 6, 'La contrasena debe tener al menos 6 caracteres' ],
+        // max: [ 12, 'La contrasena debe tener maxico 12 caracteres' ],
         required: [ true, 'La contrasena es obligatoria' ]
-    },
-    role: {
-        type: String,
-        enum: [ 'admin', 'especialista', 'paciente' ],
-        default: 'paciente'
-    }
-},{
-    timestamps: true,           // Crea dos campos 'createdAt', 'updatedAt'
-    versionKey: false           // Contador de la version del documento
-});
-
-// Definir el Modelo
-const userModel = mongoose.model( 
-    'users',            // Define el nombre la coleccion donde se guardaran los documentos
-    userSchema
-);
-
-
-// Exponemos el modelo al resto de la aplicacion
-=======
-        required: [true, 'La contraseña es obligatoria.'],
-        // minlength: [6, 'La contraseña debe tener al menos 6 caracteres.'],
-        // maxlength: [12, 'La contraseña debe tener máximo 12 caracteres.'],
     },
     role: {
         type: String,
@@ -92,5 +52,5 @@ const userModel = mongoose.model(
     userSchema          // Estructura de datos definida para este documento
 );
 
->>>>>>> feature/authentication
+
 export default userModel;
