4b41076fd27021518ec96b66ab40ccc64a378074
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-08-14 16:27:59 -0500

    [+] user.controller.test.js: Define test 'Debe fallar si el email es invalido' (Validacion directa del Schema)

--- src/__tests__/controllers/user.controller.test.mjs
+++ src/__tests__/controllers/user.controller.test.mjs
@@ -17,4 +17,21 @@ describe('Validaciones de UserSchema', () => {
             expect(error.errors.name.message).toBe('El nombre del usuario es obligatorio.');
         }
     });
+
+    test( 'Debe fallar si el campo "email" es inválido', async () => {
+        const user = new userModel({
+            name: 'Manuela',
+            username: 'manu',
+            email: 'correo.co', // Email inválido
+            password: '123456',
+            role: 'registered'
+        });
+
+        try {
+            await user.validate();
+        } catch (error) {
+            expect(error.errors.email).toBeDefined();
+            expect(error.errors.email.message).toBe('Por favor, introduce un correo electrónico válido.');
+        }
+    });
 });
